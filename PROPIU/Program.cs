6fb5546ac644e1fb8cd4c4f323b5faa36ee1ed6a
rZn24h <razvanhertanu@gmail.com>
2023-05-23 18:59:49 +0300

    L3- ADD

--- PROPIU/Program.cs
+++ PROPIU/Program.cs
@@ -1,16 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PROPIU
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        AdministrareJocuri administrareJocuri = new AdministrareJocuri();
 
+        bool meniuActiv = true;
+
+        while (meniuActiv)
+        {
+            Console.WriteLine("===== MENIU =====");
+            Console.WriteLine("1. Adăugare joc");
+            Console.WriteLine("2. Salvare jocuri în fișier");
+            Console.WriteLine("3. Preluare jocuri din fișier");
+            Console.WriteLine("4. Căutare joc");
+            Console.WriteLine("5. Afișare jocuri");
+            Console.WriteLine("6. Ieșire");
+
+            Console.Write("Selectați o opțiune: ");
+            string optiune = Console.ReadLine();
+
+            Console.WriteLine();
+
+            switch (optiune)
+            {
+                case "1":
+                    administrareJocuri.AdaugareJoc();
+                    break;
+                case "2":
+                    Console.Write("Introduceți numele fișierului: ");
+                    string numeFisierSalvare = Console.ReadLine();
+                    administrareJocuri.SalvareInFisier(numeFisierSalvare);
+                    break;
+                case "3":
+                    Console.Write("Introduceți numele fișierului: ");
+                    string numeFisierPreluare = Console.ReadLine();
+                    administrareJocuri.PreluareDinFisier(numeFisierPreluare);
+                    break;
+                case "4":
+                    administrareJocuri.CautareJoc();
+                    break;
+                case "5":
+                    administrareJocuri.AfiseazaJocuri();
+                    break;
+                case "6":
+                    meniuActiv = false;
+                    break;
+                default:
+                    Console.WriteLine("Opțiunea introdusă nu este validă.");
+                    break;
+            }
+
+            Console.WriteLine();
         }
+
+        Console.WriteLine("Program încheiat.");
     }
 }
